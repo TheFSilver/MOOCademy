0fc5d516ad2b5e757cdefe2d43c570ca7d2ccc56
TheFSilver <dasylva.f@gmail.com>
2018-07-25 18:18:13 +0200

    Code update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# Creation de 10 Cours avec un nombre random de leçons pour chacun
+10.times do
+  course = Course.create(title: Faker::Book.title, description: Faker::OnePiece.quote)
+  rand(4..7).times do
+    lesson = Lesson.create(course_id: course.id, title: Faker::Book.title, body: Faker::OnePiece.quote)
+  end
+end
